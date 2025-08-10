e3a873dbf7c020e2c959761f91d08373d8e4c9a6
Aviral Ale <aviralale@gmail.com>
2025-08-10 11:45:56 +0545

    added team section

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import HeroSection from "@/components/home/hero-section";
 import ServicesSection from "@/components/home/services-section";
 import AboutSection from "@/components/home/about-section";
 import CTASection from "@/components/home/cta-section";
-import TeamSection from "@/components/home/founder-section";
 import CTASection1 from "@/components/home/cta-1-section";
+import FounderSection from "@/components/home/founder-section";
+import TeamSection from "@/components/home/team-section";
 
 const DigicallyHomepage = () => {
   return (
@@ -13,6 +14,7 @@ const DigicallyHomepage = () => {
       <ServicesSection />
       <AboutSection />
       <CTASection />
+      <FounderSection />
       <TeamSection />
       <CTASection1 />
     </div>
